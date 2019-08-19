6eee5b634e6d9a885734c860d257e70be1503ed5
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-08-18 19:27:27 -0500

    Resolvendo problema do 'Ver mais' na descriçao do plugin.

--- ExcecuteDoteNetRun.bat
+++ ExcecuteDoteNetRun.bat
@@ -2,8 +2,9 @@
 title YourTitle
 color D
 
-echo =========== INICIANDO BUKKIT DEV SYSTEM WEB =============
+echo =========== INICIANDO BUKKIT DEV WEB =============
 
 cd Web
 dotnet run --project Web.csproj
+dotnet dev-certs https --trust
 pause
