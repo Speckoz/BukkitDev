5a9e72e528271666cbb9b21077f70c4e10773269
marcopandolfo <marcopandolfo577@gmail.com>
2019-08-22 14:31:57 -0300

    Class Purchase service

---
+++ Web/Services/PurchaseService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Services
+{
+    public static class PurchaseService
+    {
+        // POST CreatePayment
+        static public object CreatePayment(int pluginId)
+        {
+            RestClient client = new RestClient(@"http://localhost:3000/CreatePayment");
+            var request = new RestRequest(Method.POST);
+            string json = JsonConvert.SerializeObject(pluginId);
+            request.AddHeader("Accept", "application/json");
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(json);
+            IRestResponse response = client.Execute(request);
+            return JsonConvert.DeserializeObject<object>(response.Content);
+        }
+    }
+}
