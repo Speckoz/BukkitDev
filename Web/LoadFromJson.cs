b4e7bca0f62e8bfea6ad38550abf2a29146e3b9e
marcopandolfo <marcopandolfo577@gmail.com>
2019-08-22 18:00:45 -0300

    Bug

---
+++ Web/LoadFromJson.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Web
+{
+    public static class LoadFromJson
+    {
+        public static string Foo()
+        {
+            JObject o = new JObject();
+            o["Links"] = File.ReadAllText("appsettings.json");
+            Console.WriteLine(o.ToString());
+            return o.ToString();
+        }
+    }
+}
