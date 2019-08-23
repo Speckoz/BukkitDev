823e79ff2efd487ed35d0a356a99bf1163ea4a7d
marcopandolfo <marcopandolfo577@gmail.com>
2019-08-23 20:56:35 -0300

    Configuration Service

---
+++ Web/Services/ConfigurationService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Web.Services
+{
+    /// <summary>
+    /// Serviço para recuperar configurações do appsettings
+    /// </summary>
+    public class ConfigurationService
+    {
+        /// <summary>
+        /// Recupera os dados do appsettings
+        /// </summary>
+        /// <param name="dad">Nome do nó pai no json</param>
+        /// <param name="children">Nome do nó filho no json</param>
+        /// <returns></returns>
+        public static string NodeJsonValue(string dad, string children)
+        {
+            return GetJson().GetSection(dad).GetSection(children).Value;
+        }
+
+        /// <summary>
+        /// Lê o arquivo appsettings
+        /// </summary>
+        private static IConfigurationRoot GetJson()
+        {
+            var b = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true);
+            return b.Build();
+        }
+    }
+}
