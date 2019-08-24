ad03cdf1598b44c4ec975c2895785df74d41ae08
marcopandolfo <marcopandolfo577@gmail.com>
2019-08-24 00:25:47 -0300

    Doc

--- Web/Models/PurchaseModel.cs
+++ Web/Models/PurchaseModel.cs
@@ -2,8 +2,17 @@
 {
     public class PurchaseModel
     {
+        /// <summary>
+        /// Link de pagamento.
+        /// </summary>
         public string Link { get; set; }
+        /// <summary>
+        /// ID da compra.
+        /// </summary>
         public string Id { get; set; }
+        /// <summary>
+        /// Descrição da compra.
+        /// </summary>
         public string Description { get; set; }
     }
 }
